115cec32747c14a4ccd9fc86d5182cb32e998d00
Citrus <savelijpetrov38@gmail.com>
2024-06-23 23:25:43 +0300

    Add tests config

---
+++ UserService.Persistance/Migrations/20240623184856_GroupLimitsCorrection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class GroupLimitsCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ UserService.Tests/Common/CommonTest.cs
@@ -0,0 +1,21 @@
+﻿using AutoFixture;
+using UserService.Persistance;
+
+namespace UserService.Tests.Common;
+
+public class CommonTest : IDisposable
+{
+    protected readonly AppDbContext Context;
+    protected readonly Fixture Fixture;
+
+    public CommonTest()
+    {
+        Context = ContextFactory.Create();
+        Fixture = new Fixture();
+    }
+
+    public void Dispose()
+    {
+        ContextFactory.Destroy(Context);
+    }
+}
