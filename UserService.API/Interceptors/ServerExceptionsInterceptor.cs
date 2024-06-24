83c78316a1c050e580a2dfda8c618bcd4441cdc7
Citrus <savelijpetrov38@gmail.com>
2024-06-24 21:08:11 +0300

    Add server global exceptions handler

---
+++ UserService.API/Interceptors/ServerExceptionsInterceptor.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+using Serilog;
+
+namespace UserService.API.Interceptors;
+
+public class ServerExceptionsInterceptor : Interceptor
+{
+    public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request,
+        ServerCallContext context,
+        UnaryServerMethod<TRequest, TResponse> continuation)
+    {
+        try
+        {
+            return await continuation(request, context);
+        }
+        catch (Exception e)
+        {
+            switch (e)
+            {
+                case RpcException rpc:
+                    Log.Error(rpc.Status.Detail);
+                    break;
+                default:
+                    Log.Error(e.Message);
+                    break;
+            }
+
+            throw;
+        }
+    }
+}
