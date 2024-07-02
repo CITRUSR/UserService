54d52fc1a60bb65b49426d5d8cf12a6158b0b2f2
Citrus <savelijpetrov38@gmail.com>
2024-07-02 23:47:07 +0300

    Add redis extension

---
+++ UserService.Persistance/Extensions/RedisExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace UserService.Persistance.Extensions;
+
+public static class RedisExtension
+{
+    public static async Task<T> GetOrCreateAsync<T>(this IDistributedCache cache, string key,
+        Func<IRequest, CancellationToken, T> fact,
+        IRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        var cachedValue = await cache.GetStringAsync(key, cancellationToken);
+
+        T? value;
+        if (!string.IsNullOrWhiteSpace(cachedValue))
+        {
+            value = JsonSerializer.Deserialize<T>(cachedValue);
+
+            if (value != null)
+            {
+                return value;
+            }
+        }
+
+        value = fact(request, cancellationToken);
+
+        await cache.SetStringAsync(key, JsonSerializer.Serialize(value), cancellationToken);
+        
+        return value;
+    }
+}
