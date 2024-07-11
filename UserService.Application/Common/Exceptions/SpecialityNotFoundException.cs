4e73f36c0f5ccfb70b745fa0b8a3eac3c40575fc
Citrus <savelijpetrov38@gmail.com>
2024-07-11 15:05:32 +0300

    Add Speciality and Teacher not found exceptions

---
+++ UserService.Application/Common/Exceptions/SpecialityNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Core;
+
+namespace UserService.Application.Common.Exceptions;
+
+public class SpecialityNotFoundException : RpcException
+{
+    public SpecialityNotFoundException(int id) : base(new Status(StatusCode.NotFound,
+        $"The speciality with id:{id} is not found"))
+    {
+    }
+}
---
+++ UserService.Application/Common/Exceptions/TeacherNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Core;
+
+namespace UserService.Application.Common.Exceptions;
+
+public class TeacherNotFoundException : RpcException
+{
+    public TeacherNotFoundException(Guid id) : base(new Status(Grpc.Core.StatusCode.NotFound,
+        $"The teacher with id:{id} is not found"))
+    {
+    }
+}
