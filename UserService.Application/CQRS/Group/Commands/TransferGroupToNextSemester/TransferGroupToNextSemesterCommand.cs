7b5809d5576f96550e51c7fc02285de92d76322c
Citrus <savelijpetrov38@gmail.com>
2024-07-13 23:53:46 +0300

    Remove transfer group to next semester functional

--- UserService.Application/CQRS/Group/Commands/TransferGroupToNextSemester/TransferGroupToNextSemesterCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace UserService.Application.CQRS.Group.Commands.TransferGroupToNextSemester;
-
-public record TransferGroupToNextSemesterCommand(int GroupId) : IRequest<int>;
