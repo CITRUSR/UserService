3793a8cfbc5ad3cf57bb86ede31325c34fdea974
Citrus <savelijpetrov38@gmail.com>
2024-07-13 23:52:27 +0300

    Add GroupCourseOutOfRangeException

---
+++ UserService.Application/Common/Exceptions/GroupCourseOutOfRangeException.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using Grpc.Core;
+using UserService.Application.Common.Errors;
+
+namespace UserService.Application.Common.Exceptions;
+
+public class GroupCourseOutOfRangeException : RpcException
+{
+    public GroupCourseOutOfRangeException(int[] id) : base(new Status(StatusCode.OutOfRange,
+        JsonSerializer.Serialize(new Error
+        {
+            Erorrs =
+            [
+                "The groups with id:" + String.Join(", ", id) +
+                " cannot be promoted to a higher course because it is already at the maximum course."
+            ]
+        })))
+    {
+    }
+}
