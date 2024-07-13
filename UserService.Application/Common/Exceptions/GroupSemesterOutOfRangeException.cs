72fb2f27940939e460396753a45f29d1804ca49c
Citrus <savelijpetrov38@gmail.com>
2024-07-13 23:52:42 +0300

    Add GroupSemesterOutOfRangeException

---
+++ UserService.Application/Common/Exceptions/GroupSemesterOutOfRangeException.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using Grpc.Core;
+using UserService.Application.Common.Errors;
+
+namespace UserService.Application.Common.Exceptions;
+
+public class GroupSemesterOutOfRangeException : RpcException
+{
+    public GroupSemesterOutOfRangeException(int[] groupsId) : base(new Status(StatusCode.OutOfRange,
+        JsonSerializer.Serialize(new Error
+        {
+            Erorrs =
+            [
+                "The groups with id:" + String.Join(", ", groupsId) +
+                " cannot be promoted to a higher semester because it is already at the maximum semester."
+            ]
+        })))
+    {
+    }
+}
