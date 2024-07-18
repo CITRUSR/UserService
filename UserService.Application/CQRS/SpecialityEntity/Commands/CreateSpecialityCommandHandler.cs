063c195b614c412b8cd7feb7045c25e3a778a8f9
CITRUSR <savelypetrov2007@mail.ru>
2024-07-18 18:03:42 +0300

    Add create speciality functional

---
+++ UserService.Application/CQRS/SpecialityEntity/Commands/CreateSpecialityCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using UserService.Domain.Entities;
+
+namespace UserService.Application.CQRS.SpecialityEntity.Commands;
+
+public class CreateSpecialityCommandHandler(IAppDbContext dbContext)
+    : HandlerBase(dbContext), IRequestHandler<CreateSpecialityCommand, int>
+{
+    public async Task<int> Handle(CreateSpecialityCommand request, CancellationToken cancellationToken)
+    {
+        var speciality = new Speciality()
+        {
+            DurationMonths = request.DurationMonths,
+            Cost = request.Cost,
+            Name = request.Name,
+            Abbreavation = request.Abbreavation,
+        };
+
+        await DbContext.Specialities.AddAsync(speciality, cancellationToken);
+        await DbContext.SaveChangesAsync(cancellationToken);
+
+        return speciality.Id;
+    }
+}
