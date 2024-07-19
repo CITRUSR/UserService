64d35414d94d883dca6d4ba4e8d9fa36962140f0
CITRUSR <savelypetrov2007@mail.ru>
2024-07-19 17:09:50 +0300

    Add speciality mapper

---
+++ UserService.API/Mappers/SpecialityMapper.cs
@@ -0,0 +1,25 @@
+﻿using UserService.Domain.Entities;
+
+namespace UserService.API.Mappers;
+
+public class SpecialityMapper : IMapper<Speciality, SpecialityModel>
+{
+    public SpecialityModel Map(Speciality from)
+    {
+        CustomTypes.DecimalValue constDec = from.Cost;
+        var cost = new DecimalValue
+        {
+            Units = constDec.Units,
+            Nanos = constDec.Nanos
+        };
+        return new SpecialityModel
+        {
+            Id = from.Id,
+            Name = from.Name,
+            Abbreavation = from.Abbreavation,
+            Cost = cost,
+            DurationMonths = from.DurationMonths,
+            IsDeleted = from.IsDeleted,
+        };
+    }
+}
