a549ba3e91319c22615207f73281c00691970c60
CITRUSR <savelypetrov2007@mail.ru>
2024-07-22 17:58:37 +0300

    bug fix redis extension and change base json to newtonsoft

---
+++ UserService.Application/extensions/RedisExtension.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace UserService.Persistance.Extensions;
+
+public static class RedisExtension
+{
+    public static async Task<TResponse> GetOrCreateAsync<TQuery, TResponse>(this IDistributedCache cache, string key,
+        Func<TQuery, CancellationToken, Task<TResponse>> fact,
+        TQuery request,
+        CancellationToken cancellationToken = default) where TQuery : IRequest<TResponse>
+    {
+        var cachedValue = await cache.GetStringAsync(key, cancellationToken);
+
+        TResponse? value;
+        if (!string.IsNullOrWhiteSpace(cachedValue))
+        {
+            value = JsonConvert.DeserializeObject<TResponse>(cachedValue);
+
+            if (value != null)
+            {
+                return value;
+            }
+        }
+
+        value = await fact(request, cancellationToken);
+
+        await cache.SetStringAsync(key, JsonConvert.SerializeObject(value), cancellationToken);
+
+        return value;
+    }
+}
