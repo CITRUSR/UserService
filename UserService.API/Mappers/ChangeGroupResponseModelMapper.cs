566516108dd03d0e787265968ada26a4fb97c391
CITRUSR <savelypetrov2007@mail.ru>
2024-07-30 16:39:04 +0300

    Add change group response model mapper

---
+++ UserService.API/Mappers/ChangeGroupResponseModelMapper.cs
@@ -0,0 +1,17 @@
+﻿using UserService.Domain.Entities;
+
+namespace UserService.API.Mappers;
+
+public class ChangeGroupResponseModelMapper : IMapper<Group, ChangeGroupResponseModel>
+{
+    public ChangeGroupResponseModel Map(Group from)
+    {
+        return new ChangeGroupResponseModel
+        {
+            Abbr = from.Speciality.Abbreavation,
+            CurrentCourse = from.CurrentCourse,
+            SubGroup = from.SubGroup,
+            Id = from.Id,
+        };
+    }
+}
