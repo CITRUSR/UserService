d6bfe11a62efa5ee3ccb4cfe085682ac6da4cd8a
CITRUSR <savelypetrov2007@mail.ru>
2024-07-30 16:42:16 +0300

    update groups course out of range exception(add more details)

--- UserService.Application/Common/Exceptions/GroupCourseOutOfRangeException.cs
+++ UserService.Application/Common/Exceptions/GroupCourseOutOfRangeException.cs
@@ -1,17 +1,19 @@
 ﻿using System.Text.Json;
 using Grpc.Core;
 using UserService.Application.Common.Errors;
+using UserService.Domain.Entities;
 
 namespace UserService.Application.Common.Exceptions;
 
 public class GroupCourseOutOfRangeException : RpcException
 {
-    public GroupCourseOutOfRangeException(int[] id) : base(new Status(StatusCode.OutOfRange,
+    public GroupCourseOutOfRangeException(Group[] groups) : base(new Status(StatusCode.OutOfRange,
         JsonSerializer.Serialize(new Error
         {
             Erorrs =
             [
-                "The groups with id:" + String.Join(", ", id) +
+                "The groups:" + String.Join(", ",
+                    groups.Select(x => x.CurrentCourse + "-" + x.Speciality.Abbreavation + x.SubGroup)) +
                 " cannot be promoted to a higher course because it is already at the maximum course."
             ]
         })))
