007b723359058b1e9f994546f5ac88ee46b0f119
CITRUSR <savelijpetrov38@gmail.com>
2024-08-03 00:38:21 +0300

    update group semester out of range exception

--- UserService.Application/Common/Exceptions/GroupSemesterOutOfRangeException.cs
+++ UserService.Application/Common/Exceptions/GroupSemesterOutOfRangeException.cs
@@ -7,13 +7,13 @@ namespace UserService.Application.Common.Exceptions;
 
 public class GroupSemesterOutOfRangeException : RpcException
 {
-    public GroupSemesterOutOfRangeException(Group[] groups) : base(new Status(StatusCode.OutOfRange,
+    public GroupSemesterOutOfRangeException(params Group[] groups) : base(new Status(StatusCode.OutOfRange,
         JsonSerializer.Serialize(new Error
         {
             Erorrs =
             [
                 "The groups:" + String.Join(", ",
-                    groups.Select(x => x.CurrentCourse + "-" + x.Speciality.Abbreavation + x.SubGroup)) +
+                    groups.Select(x => x.ToString())) +
                 " cannot be promoted to a higher semester because it is already at the maximum semester."
             ]
         })))
