37c962dedd25fa599410a692cdd06ea1a1344ccd
CITRUSR <savelijpetrov38@gmail.com>
2024-08-03 16:32:39 +0300

    update graduate groups functional

---
+++ UserService.Application/Common/Exceptions/GroupAlreadyGraduatedException.cs
@@ -0,0 +1,22 @@
+using System.Text.Json;
+using Grpc.Core;
+using UserService.Application.Common.Errors;
+using UserService.Domain.Entities;
+
+namespace UserService.Application.Common.Exceptions;
+
+public class GroupAlreadyGraduatedException : RpcException
+{
+    public GroupAlreadyGraduatedException(params Group[] groups) : base(new Status(StatusCode.Aborted,
+     JsonSerializer.Serialize(new Error
+     {
+         Erorrs =
+            [
+                "The groups:" + String.Join(", ",
+                    groups.Select(x => x.ToString())) +
+                " cannot be graduated because it is already graduated."
+            ]
+     })))
+    {
+    }
+}
