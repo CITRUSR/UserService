cac9dc6b84a836e6715ffe146904264474b367ee
CITRUSR <savelypetrov2007@mail.ru>
2024-08-04 12:41:23 +0300

    add graduate groups cached functional

---
+++ UserService.Application/CQRS/GroupEntity/Commands/GraduateGroups/GraduateGroupsCommandHandlerCached.cs
@@ -0,0 +1,39 @@
+using MediatR;
+using UserService.Application.Abstraction;
+using UserService.Application.Common.Cache;
+using UserService.Domain.Entities;
+
+namespace UserService.Application.CQRS.GroupEntity.Commands.GraduateGroups;
+
+public class GraduateGroupsCommandHandlerCached(
+    ICacheService cacheService,
+    GraduateGroupsCommandHandler handler
+) : IRequestHandler<GraduateGroupsCommand, List<Group>>
+{
+    private readonly ICacheService _cacheService = cacheService;
+    private readonly GraduateGroupsCommandHandler _handler = handler;
+
+    public async Task<List<Group>> Handle(
+        GraduateGroupsCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        var groups = await _handler.Handle(request, cancellationToken);
+
+        foreach (var group in groups)
+        {
+            await _cacheService.SetObjectAsync<Group>(
+                CacheKeys.ById<Group, int>(group.Id),
+                group,
+                cancellationToken
+            );
+            await _cacheService.RemovePagesWithObjectAsync<Group, int>(
+                group.Id,
+                (gr, i) => gr.Id == i,
+                cancellationToken
+            );
+        }
+
+        return groups;
+    }
+}
