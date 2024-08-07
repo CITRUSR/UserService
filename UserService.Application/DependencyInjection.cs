0fcd8153384f68790234703225cbb2b9765f606b
CITRUSR <savelypetrov2007@mail.ru>
2024-08-07 19:40:28 +0300

    add delete speciality cached functional

--- UserService.Application/DependencyInjection.cs
+++ UserService.Application/DependencyInjection.cs
@@ -14,6 +14,7 @@ using UserService.Application.CQRS.GroupEntity.Commands.TransferGroupsToNextCour
 using UserService.Application.CQRS.GroupEntity.Commands.TransferGroupsToNextSemester;
 using UserService.Application.CQRS.GroupEntity.Queries.GetGroupById;
 using UserService.Application.CQRS.GroupEntity.Queries.GetGroups;
+using UserService.Application.CQRS.SpecialityEntity.Commands.DeleteSpeciality;
 using UserService.Application.CQRS.SpecialityEntity.Queries.GetSpecialities;
 using UserService.Application.CQRS.SpecialityEntity.Queries.GetSpecialityById;
 using UserService.Domain.Entities;
@@ -81,6 +82,10 @@ public static class DependencyInjection
         services.Decorate<
             IRequestHandler<GetSpecialitiesQuery, PaginationList<Speciality>>,
             GetSpecialitiesQueryHandlerCached
+        >();
+        services.Decorate<
+            IRequestHandler<DeleteSpecialityCommand, List<Speciality>>,
+            DeleteSpecialityCommandHandlerCached
         >();
     }
 }
---
+++ UserService.Tests/Entities/SpecialityEntity/Commands/DeleteSpecialityCached.cs
@@ -0,0 +1,38 @@
+using FluentAssertions;
+using UserService.Application.Common.Cache;
+using UserService.Application.CQRS.SpecialityEntity.Commands.DeleteSpeciality;
+using UserService.Domain.Entities;
+using UserService.Tests.Common;
+
+namespace UserService.Tests.Entities.SpecialityEntity.Commands;
+
+public class DeleteSpecialityCached(DatabaseFixture databaseFixture) : RedisTest(databaseFixture)
+{
+    [Fact]
+    public async void DeleteSpecialityCached_ShouldBe_Success()
+    {
+        var speciality = Fixture.Create<Speciality>();
+
+        await AddSpecialitiesToContext(speciality);
+
+        var command = new DeleteSpecialityCommand([speciality.Id]);
+
+        var handler = new DeleteSpecialityCommandHandlerCached(
+            CacheService,
+            new DeleteSpecialityCommandHandler(Context)
+        );
+
+        var specialities = await handler.Handle(command, CancellationToken.None);
+
+        foreach (var s in specialities)
+        {
+            var key = CacheKeys.ById<Speciality, int>(s.Id);
+
+            var cachedString = await CacheService.GetStringAsync(key);
+            var cachedSpeciality = await CacheService.GetObjectAsync<Speciality>(key);
+
+            cachedString.Should().BeNull();
+            cachedSpeciality.Should().BeNull();
+        }
+    }
+}
