cbad1e002d2ce82f69d9e0aee427f49f8a44d358
CITRUSR <savelypetrov2007@mail.ru>
2024-08-07 18:37:33 +0300

    update speciality not found exception for multiply usage

--- UserService.Application/Common/Exceptions/SpecialityNotFoundException.cs
+++ UserService.Application/Common/Exceptions/SpecialityNotFoundException.cs
@@ -4,6 +4,11 @@ namespace UserService.Application.Common.Exceptions;
 
 public class SpecialityNotFoundException : RpcException
 {
-    public SpecialityNotFoundException(int id)
-        : base(new Status(StatusCode.NotFound, $"The speciality with id:{id} is not found")) { }
+    public SpecialityNotFoundException(params int[] ids)
+        : base(
+            new Status(
+                StatusCode.NotFound,
+                $"The specialities with id:{string.Join(", ", ids)} are not found"
+            )
+        ) { }
 }
