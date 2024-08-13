86c4483e08663e7ff39357aa47a75022be807683
CITRUSR <savelypetrov2007@mail.ru>
2024-08-13 19:23:50 +0300

    extract some ligic from class

--- UserService.Tests/Common/RedisTest.cs
+++ UserService.Tests/Common/RedisTest.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Testcontainers.Redis;
 using UserService.Application.Abstraction;
 using UserService.Application.Common.Cache;
+using UserService.Tests.Common.Factories;
 
 namespace UserService.Tests.Common;
 
@@ -10,48 +11,27 @@ public class RedisTest(DatabaseFixture databaseFixture)
     : CommonTest(databaseFixture),
         IAsyncLifetime
 {
-    protected ICacheService CacheService { get; set; }
-
+    protected ICacheService CacheService { get; private set; }
     private RedisCache _redisCache;
-    private RedisContainer _redisContainer;
+    private RedisContainer _redisContainer = new RedisBuilder()
+        .WithImage("redis:7.4-rc")
+        .WithExposedPort("6379")
+        .WithHostname("redis")
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(6379))
+        .Build();
 
-    public async Task InitializeAsync()
+    public new async Task InitializeAsync()
     {
-        _redisContainer = CreateRedisContainer();
-
         await _redisContainer.StartAsync();
 
-        _redisCache = CreateRedisCache();
+        _redisCache = RedisFactory.Create(_redisContainer.GetMappedPublicPort("6379"));
         CacheService = new CacheService(_redisCache);
     }
 
-    public async Task DisposeAsync()
+    public new async Task DisposeAsync()
     {
         _redisCache.Dispose();
 
         await _redisContainer.DisposeAsync();
-    }
-
-    private RedisContainer CreateRedisContainer()
-    {
-        return new RedisBuilder()
-            .WithImage("redis:7.4-rc")
-            .WithExposedPort("6379")
-            .WithHostname("redis")
-            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(6379))
-            .Build();
-    }
-
-    private RedisCache CreateRedisCache()
-    {
-        var redisPort = _redisContainer.GetMappedPublicPort("6379");
-
-        RedisCacheOptions options = new RedisCacheOptions
-        {
-            Configuration = $"localhost:{redisPort}",
-            InstanceName = "MyRedisCache",
-        };
-
-        return new RedisCache(options);
     }
 }
