1f5ea32925bde0a46516e22214d7dbade91833d3
CITRUSR <savelypetrov2007@mail.ru>
2024-08-23 22:46:46 +0300

    update create student cache invalidation

--- UserService.Application/CQRS/StudentEntity/Commands/CreateStudent/CreateStudentCommandHandlerCached.cs
+++ UserService.Application/CQRS/StudentEntity/Commands/CreateStudent/CreateStudentCommandHandlerCached.cs
@@ -22,8 +22,9 @@ public class CreateStudentCommandHandlerCached(
 
         for (int i = 0; i < CacheConstants.PagesForCaching; i++)
         {
-            await _cacheService.RemoveAsync(
-                CacheKeys.GetEntities<Student>(i, 10),
+            await _cacheService.RemovePagesWithObjectAsync<Student, Guid>(
+                id,
+                (student, i) => student.Id == i,
                 cancellationToken
             );
         }
