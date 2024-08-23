7d046a79d6ac1e6773a7ead88bd43150aa7daecd
CITRUSR <savelypetrov2007@mail.ru>
2024-08-23 23:38:42 +0300

    add get group by id cached

--- UserService.Application/DependencyInjection.cs
+++ UserService.Application/DependencyInjection.cs
@@ -25,6 +25,7 @@ using UserService.Application.CQRS.StudentEntity.Commands.CreateStudent;
 using UserService.Application.CQRS.StudentEntity.Commands.DeleteStudent;
 using UserService.Application.CQRS.StudentEntity.Commands.DropOutStudent;
 using UserService.Application.CQRS.StudentEntity.Commands.EditStudent;
+using UserService.Application.CQRS.StudentEntity.Queries.GetStudentById;
 using UserService.Domain.Entities;
 
 namespace UserService.Application;
@@ -140,6 +141,10 @@ public static class DependencyInjection
         services.Decorate<
             IRequestHandler<EditStudentCommand, Student>,
             EditStudentCommandHandlerCached
+        >();
+        services.Decorate<
+            IRequestHandler<GetStudentByIdQuery, Student>,
+            GetStudentByIdQueryHandlerCached
         >();
     }
 }
