616647aa509fbb3f228e1d5880ac2206ace3b549
CITRUSR <savelypetrov2007@mail.ru>
2024-08-27 16:14:38 +0300

    update soft delete groups tests with mocking

--- UserService.Tests/Entities/GroupEntity/Commands/SoftDeleteGroups.cs
+++ UserService.Tests/Entities/GroupEntity/Commands/SoftDeleteGroups.cs
@@ -1,44 +1,90 @@
 using FluentAssertions;
+using Moq;
+using Moq.EntityFrameworkCore;
+using UserService.Application.Abstraction;
 using UserService.Application.Common.Exceptions;
 using UserService.Application.CQRS.GroupEntity.Commands.SoftDeleteGroups;
 using UserService.Domain.Entities;
-using UserService.Tests.Common;
 
 namespace UserService.Tests.Entities.GroupEntity.Commands;
 
-public class SoftDeleteGroups(DatabaseFixture databaseFixture) : CommonTest(databaseFixture)
+public class SoftDeleteGroups
 {
-    [Fact]
-    public async Task SoftDeleteGroups_ShouldBe_Success()
+    private readonly Mock<IAppDbContext> _mockDbContext;
+    private readonly IFixture _fixture;
+
+    public SoftDeleteGroups()
     {
-        var groups = Fixture.CreateMany<Group>(7);
-
-        await DbHelper.AddGroupsToContext([.. groups]);
-
-        var command = new SoftDeleteGroupsCommand([.. groups.Select(x => x.Id)]);
-
-        var groupsRes = await Action(command);
-
-        foreach (var group in groupsRes)
-        {
-            group.IsDeleted.Should().BeTrue();
-        }
+        _mockDbContext = new Mock<IAppDbContext>();
+        _fixture = new Fixture();
     }
 
     [Fact]
-    public async Task SoftDeleteGroups_ShouldBe_GroupNotFounxException()
+    public async Task SoftDeleteGroups_ShouldBe_Success()
     {
-        var command = new SoftDeleteGroupsCommand([123, 125, 1252]);
+        _fixture.Customize<Group>(x => x.With(x => x.IsDeleted, false));
 
-        Func<Task> act = async () => await Action(command);
+        var groups = _fixture.CreateMany<Group>(3);
+
+        _mockDbContext.Setup(x => x.Groups).ReturnsDbSet([.. groups]);
+
+        var command = _fixture
+            .Build<SoftDeleteGroupsCommand>()
+            .With(x => x.GroupsId, [.. groups.Select(x => x.Id)])
+            .Create();
+
+        var handler = new SoftDeleteGroupsCommandHandler(_mockDbContext.Object);
+
+        await handler.Handle(command, CancellationToken.None);
+
+        _mockDbContext.Verify(x => x.BeginTransactionAsync(), Times.Once);
+
+        _mockDbContext.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once());
+
+        _mockDbContext.Verify(x => x.CommitTransactionAsync(), Times.Once());
+
+        groups.All(x => x.IsDeleted).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task SoftDeleteGroups_ShouldBe_GroupNotFoundException_WhenGroupsDoNotExist()
+    {
+        _mockDbContext.Setup(x => x.Groups).ReturnsDbSet([]);
+
+        var command = _fixture
+            .Build<SoftDeleteGroupsCommand>()
+            .With(x => x.GroupsId, [123, 124, 523])
+            .Create();
+
+        var handler = new SoftDeleteGroupsCommandHandler(_mockDbContext.Object);
+
+        Func<Task> act = async () => await handler.Handle(command, CancellationToken.None);
 
         await act.Should().ThrowAsync<GroupNotFoundException>();
     }
 
-    private async Task<List<Group>> Action(SoftDeleteGroupsCommand command)
+    [Fact]
+    public async Task SoftDeleteGroups_ShouldBe_CallRallback_WhenThrowException()
     {
-        var handler = new SoftDeleteGroupsCommandHandler(Context);
+        var groups = _fixture.CreateMany<Group>(3);
 
-        return await handler.Handle(command, CancellationToken.None);
+        _mockDbContext.Setup(x => x.Groups).ReturnsDbSet([.. groups]);
+
+        _mockDbContext
+            .Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            .Throws(new Exception());
+
+        var command = _fixture
+            .Build<SoftDeleteGroupsCommand>()
+            .With(x => x.GroupsId, [.. groups.Select(x => x.Id)])
+            .Create();
+
+        var handler = new SoftDeleteGroupsCommandHandler(_mockDbContext.Object);
+
+        Func<Task> act = async () => await handler.Handle(command, CancellationToken.None);
+
+        await act.Should().ThrowAsync<Exception>();
+
+        _mockDbContext.Verify(x => x.RollbackTransactionAsync(), Times.Once());
     }
 }
