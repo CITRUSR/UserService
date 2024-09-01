46d99d78b16dbb9c41452cea510f9c17d36bc6d6
CITRUSR <savelypetrov2007@mail.ru>
2024-09-01 23:24:53 +0300

    add mapster configuration for group short info dto

--- UserService.Application/MapsterConfig.cs
+++ UserService.Application/MapsterConfig.cs
@@ -1,6 +1,15 @@
+using Mapster;
+using UserService.Application.CQRS.GroupEntity.Responses;
+using UserService.Domain.Entities;
+
 namespace UserService.Application;
 
 public static class MapsterConfig
 {
-    public static void Configure() { }
+    public static void Configure()
+    {
+        TypeAdapterConfig<Group, GroupShortInfoDto>
+            .NewConfig()
+            .Map(dest => dest.GroupName, src => src.ToString());
+    }
 }
