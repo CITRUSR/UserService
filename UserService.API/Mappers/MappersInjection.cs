49779b7341842435910287517216d5f361fa19eb
CITRUSR <savelypetrov2007@mail.ru>
2024-09-08 01:31:47 +0300

    remove speciality mapper

--- UserService.API/Mappers/MappersInjection.cs
+++ UserService.API/Mappers/MappersInjection.cs
@@ -7,7 +7,6 @@ public static class MappersInjection
     public static IServiceCollection AddMappers(this IServiceCollection services)
     {
         services.AddSingleton<IMapper<Group, GroupModel>, GroupMapper>();
-        services.AddSingleton<IMapper<Speciality, SpecialityModel>, SpecialityMapper>();
         services.AddSingleton<
             IMapper<Group, ChangeGroupResponseModel>,
             ChangeGroupResponseModelMapper
