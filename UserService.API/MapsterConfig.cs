a244f06aa4db28dd91c555f96b49d0732bc8ca33
CITRUSR <savelypetrov2007@mail.ru>
2024-09-14 23:25:40 +0300

    add mapster config for delete groups response

--- UserService.API/MapsterConfig.cs
+++ UserService.API/MapsterConfig.cs
@@ -1,6 +1,14 @@
+using Mapster;
+using UserService.Application.CQRS.GroupEntity.Responses;
+
 namespace UserService.API;
 
 public static class MapsterConfig
 {
-    public static void Configure() { }
+    public static void Configure()
+    {
+        TypeAdapterConfig<List<GroupShortInfoDto>, DeleteGroupsResponse>
+            .NewConfig()
+            .Map(dest => dest.Groups, src => src);
+    }
 }
