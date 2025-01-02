36e63ee68eb709ebc7c300c745ac48900f179f10
CITRUSR <savelypetrov2007@mail.ru>
2025-01-03 00:40:46 +0300

    update TypeAdapterConfig for GetStudentsByGroupIdResponse

--- UserService.API/MapsterConfig.cs
+++ UserService.API/MapsterConfig.cs
@@ -66,7 +66,10 @@ public static class MapsterConfig
             .NewConfig()
             .Map(dest => dest.Students, src => src);
 
-        TypeAdapterConfig<List<UserService.Application.CQRS.StudentEntity.Responses.StudentViewModel>, GetStudentsByGroupIdResponse>
+        TypeAdapterConfig<
+            List<Application.CQRS.StudentEntity.Responses.StudentViewModel>,
+            GetStudentsByGroupIdResponse
+        >
             .NewConfig()
             .Map(dest => dest.Students, src => src);
     }
